fbdf2b661426d11f20c153215465a04692a85172
rimey2 <abokyrimey2@gmail.com>
2024-02-08 00:50:59 +0100

    first commit

---
+++ db.js
@@ -0,0 +1,24 @@
+const { Sequelize } = require('sequelize')
+
+// Database
+const sequelize = new Sequelize(
+  process.env.DATABASE_URL,
+  {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    define: {
+      createdAt: 'added',
+      updatedAt: 'updated',
+    }
+  },
+)
+
+sequelize.authenticate()
+sequelize.sync()
+
+module.exports = sequelize
